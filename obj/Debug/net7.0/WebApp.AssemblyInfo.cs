64a3ad10fc0a5ec9cadc3b31d6c0b339d6ac2874
Connor Malcolm <connor1.mlc@gmail.com>
2024-10-15 15:25:30 +0100

    Implemented out of stock

--- obj/Debug/net7.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58cbc94834008f0481730a97fa2657781d8c134")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
